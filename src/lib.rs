c0d65ff26b0b2f31b82bc80436d7b876e6f984e3
Jacob Sullivan <jay@jaysull.dev>
2021-05-10 18:30:55 -0500

    Add comments and more thorough number parsing

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ pub enum JsonValue {
 }
 
 fn get_json_object(json_chars: &Vec<char>, position: &mut usize) -> Result<HashMap<String, Option<JsonValue>>, Box<dyn Error>> {
+    // Char will be an open curly bracket
     *position += 1;
     let mut json_obj: HashMap<String, Option<JsonValue>> = HashMap::new();
     let mut get_key: bool = true;
@@ -40,12 +41,14 @@ fn get_json_object(json_chars: &Vec<char>, position: &mut usize) -> Result<HashM
         }
     }
 
+    // Put position past closing curly bracket
     *position += 1;
 
     return Ok(json_obj);
 }
 
 fn get_json_array(json_chars: &Vec<char>, position: &mut usize) -> Result<Vec<Option<JsonValue>>, Box<dyn Error>> {
+    // Char will be an open square bracket
     *position += 1;
     let mut json_arr: Vec<Option<JsonValue>> = vec![];
 
@@ -61,12 +64,14 @@ fn get_json_array(json_chars: &Vec<char>, position: &mut usize) -> Result<Vec<Op
         }
     }
 
+    // Put position past closing square bracket
     *position += 1;
 
     return Ok(json_arr);
 }
 
 fn get_json_string(json_chars: &Vec<char>, position: &mut usize) -> Result<String, Box<dyn Error>> {
+    // Char will be an open double quotation
     *position += 1;
     let mut token = json_chars[*position];
     let mut new_string: Vec<char> = vec![];
@@ -82,25 +87,96 @@ fn get_json_string(json_chars: &Vec<char>, position: &mut usize) -> Result<Strin
         token = json_chars[*position];
     }
 
+    // Put position past closed double quotation
     *position += 1;
 
     return Ok(new_string.iter().collect::<String>());
 }
 
-fn get_json_num(json_chars: &Vec<char>, position: &mut usize) -> f64 {
+fn get_json_num(json_chars: &Vec<char>, position: &mut usize) -> Result<f64, Box<dyn Error>> {
+    // Char will be a digit or '-'
     let mut token = json_chars[*position];
-    let mut new_num: Vec<char> = vec![];
+    let mut num: Vec<char> = vec![];
+    let is_neg: bool = token == '-';
+    let mut reached_dec: bool = false;
+    let mut reached_exponent: bool = false;
+    let mut exponent_negative: bool = false;
+    let mut exponent: Vec<char> = vec![];
 
-    while token.is_digit(10) || token == '.' || token == '-' {
-        new_num.push(token);
+    if is_neg {
+        num.push(token);
         *position += 1;
         token = json_chars[*position];
     }
 
-    return new_num.iter().collect::<String>().parse().unwrap();
+    if token == '0' {
+        if 
+            !(json_chars[*position + 1] == '.' ||
+            json_chars[*position + 1] == ' ' ||
+            json_chars[*position + 1] == ',' ||
+            json_chars[*position + 1] == '\n' ||
+            json_chars[*position + 1] == '\r')
+        {
+            return Err("Invalid Json".into());
+        }
+    }
+
+    let mut done: bool = false;
+
+    while !done {
+        if token.is_digit(10) || token == '.' || token == 'e' || token == 'E' || token == '-' {
+            if token == '.' && !reached_dec && !reached_exponent {
+                num.push(token);
+                reached_dec = true;
+            } else if token == '.' && reached_dec && !reached_exponent {
+                return Err("Invalid Json".into());
+            } else if token == '-' && reached_exponent {
+                return Err("Invalid Json".into());
+            } else if (token == 'e' || token == 'E') && !reached_exponent {
+                reached_exponent = true;
+
+                if json_chars[*position + 1] == '-' {
+                    exponent_negative = true;
+                    *position += 1;
+                } else if json_chars[*position + 1] == '+' {
+                    *position += 1;
+                } else if !json_chars[*position + 1].is_digit(10) {
+                    return Err("Invalid Json".into());
+                }
+            } else if token == '-' && reached_exponent {
+                return Err("Invalid Json".into());
+            } else if (token == 'e' || token == 'E') && reached_exponent {
+                return Err("Invalid Json".into());
+            } else if token.is_digit(10) && reached_exponent {
+                exponent.push(token);
+            } else {
+                num.push(token);
+            }
+        } else if token == ' ' || token == '\n' || token == '\r' || token == ',' {
+            done = true;
+        } else {
+            return Err("Invalid Json".into());
+        }
+        
+        *position += 1;
+        token = json_chars[*position];
+    }
+
+    return Ok(
+        if reached_exponent {
+            if exponent_negative {
+                (num.iter().collect::<String>().parse::<f64>().unwrap()) / (10.0f64.powf(exponent.iter().collect::<String>().parse::<f64>().unwrap()))
+            } else {
+                (num.iter().collect::<String>().parse::<f64>().unwrap()) * (10.0f64.powf(exponent.iter().collect::<String>().parse::<f64>().unwrap()))
+            }
+        } else {
+            num.iter().collect::<String>().parse::<f64>().unwrap()
+        }
+    );
 }
 
 fn get_json_bool(json_chars: &Vec<char>, position: &mut usize, t_or_f: bool) -> Result<bool, Box<dyn Error>> {
+    // Char will be 'f' or 't'
     *position += 1;
     let false_test = vec!['a', 'l', 's', 'e'];
     let true_test = vec!['r', 'u', 'e'];
@@ -132,6 +208,7 @@ fn get_json_bool(json_chars: &Vec<char>, position: &mut usize, t_or_f: bool) -> 
 }
 
 fn check_null(json_chars: &Vec<char>, position: &mut usize) -> Result<bool, Box<dyn Error>> {
+    // Char will be 'n'
     *position += 1;
     let null_test = vec!['u', 'l', 'l'];
 
@@ -162,13 +239,13 @@ impl JsonValue {
         let value: Option<JsonValue>;
 
         if token == '"' {
-            value = Some(JsonString(get_json_string(&json_chars, position)?));
+            value = Some(JsonString(get_json_string(json_chars, position)?));
         } else if token == 'f' {
             value = Some(JsonBool(get_json_bool(json_chars, position, false)?));
         } else if token == 't' {
             value = Some(JsonBool(get_json_bool(json_chars, position, true)?));
         } else if token.is_digit(10) || token == '-' {
-            value = Some(JsonNum(get_json_num(json_chars, position)));
+            value = Some(JsonNum(get_json_num(json_chars, position)?));
         } else if token == 'n' {
             check_null(json_chars, position)?;
             value = None;
@@ -177,7 +254,7 @@ impl JsonValue {
         } else if token == '[' {
             value = Some(JsonArray(get_json_array(json_chars, position)?))
         } else {
-            return Err(format!("Invalid char: {}", token).into());
+            return Err(format!("Invalid char at position {}: {}", position, token).into());
         }
 
         return Ok(value);
